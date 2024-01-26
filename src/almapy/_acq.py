981e5f0882960c93be1e812d7fb88b2665867f78
bodl3011 <ben.gable@bodleian.ox.ac.uk>
2024-01-26 11:20:45 +0000

    feat(acq): add receive_existing_item method

--- src/almapy/_acq.py
+++ src/almapy/_acq.py
@@ -7,6 +7,8 @@ from gracy import Gracy, GracyNamespace
 from almapy._endpoints import AlmaEndpoint
 
 if TYPE_CHECKING:
+    from datetime import date
+
     from almapy._utils import RESP_TYPE
 
 
@@ -18,4 +20,33 @@ class AlmaClientAcqNS(GracyNamespace[AlmaEndpoint]):
 
     async def get_po_line(self, po_line_id: str) -> RESP_TYPE:
         resp: RESP_TYPE = await self.get(AlmaEndpoint.PO_LINE, {"PO_LINE_ID": po_line_id})
+        return resp
+
+    async def receive_existing_item(
+        self,
+        po_line_id: str,
+        item_pid: str,
+        *,
+        receive_date: date | None = None,
+        department: str | None = None,
+        department_library: str | None = None,
+        updated_item=None,
+    ) -> RESP_TYPE:
+        if updated_item is None:
+            updated_item = {}
+        params = {
+            "op": "receive",
+        }
+        if receive_date:
+            params["receive_date"] = receive_date.strftime("%Y-%m-%dZ")
+        if department:
+            params["department"] = department
+        if department_library:
+            params["department_library"] = department_library
+        resp: RESP_TYPE = await self.get(
+            AlmaEndpoint.PO_LINE_ITEM,
+            {"PO_LINE_ID": po_line_id, "ITEM_PID": item_pid},
+            params=params,
+            json=updated_item,
+        )
         return resp
