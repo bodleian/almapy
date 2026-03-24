8c7677c9db1dbd4a44afc35ecffee60a394542a3
Ben Gable <ben.gable@bodleian.ox.ac.uk>
2026-03-24 11:48:41 +0000

    feat(deps): remove gracy, export ThrottleTimeoutError
    - Remove gracy from pyproject.toml dependencies
    - stamina>=25.2.0 already present
    - Export ThrottleTimeoutError from package __init__.py
    - uv lock + sync: gracy removed from uv.lock

--- src/almapy/__init__.py
+++ src/almapy/__init__.py
@@ -1,5 +1,6 @@
 """An API wrapper library for Ex Libris' Alma."""
 
 from almapy._client import AlmaClient
+from almapy.exceptions import ThrottleTimeoutError
 
-__all__ = ["AlmaClient"]
+__all__ = ["AlmaClient", "ThrottleTimeoutError"]
